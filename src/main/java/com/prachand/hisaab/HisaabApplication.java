67d1f67d24082c26593b142f78e4d94247c61343
Pradeep Kumar Tiwari <pradeepkumartiwari@Pradeeps-MacBook-Air.local>
2024-02-10 00:46:07 +0530

    initial commit

---
+++ src/main/java/com/prachand/hisaab/HisaabApplication.java
@@ -0,0 +1,13 @@
+package com.prachand.hisaab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisaabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisaabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prachand/hisaab/HisaabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prachand.hisaab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisaabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
